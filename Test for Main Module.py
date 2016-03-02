257944d154dbf7e25efad5ee43aabe0512c2fdbe
Braedon Leonard <redirectmystuffplz@gmail.com>
2016-03-02 09:13:51 -0500

    Removed my garbage code

--- Test for Main Module.py
+++ Test for Main Module.py
@@ -1,14 +1,10 @@
 import pygame, mainModule
 from pygame.locals import *
-from mainModule import refreshScreen, setBackgroundImage
 
 size = width, height = 640, 480
 screen = pygame.display.set_mode(size)
 
-setBackgroundImage("forest.jpg")
-
-mainModule.init(screen)
 
 while True:
 
-    mainModule.refreshScreen()
+    pygame.display.flip()
--- mainModule.py
+++ mainModule.py
@@ -3,13 +3,4 @@
 import pygame
 from pygame.locals import *
 
-def init(scr):
-    screen = scr
-
-def refreshScreen():
-    screen.blit(background,(0,0))
-    pygame.display.flip()
-
-def setBackgroundImage(image):
-    background = pygame.image.load(image)
 
