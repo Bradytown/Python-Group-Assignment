3dbd20a5b64e0c7085199b3924ab621fac3977db
Braedon Leonard <redirectmystuffplz@gmail.com>
2016-03-02 09:00:34 -0500

    Added background
    Backgrounds can now be set via mainModule. refreshDisplay is made aswell

--- mainModule.py
+++ mainModule.py
@@ -1,1 +1,14 @@
 #Main module to import when you start programming
+
+import pygame
+from pygame.locals import *
+
+def __init__(screen):
+    self.screen = screen
+
+def refreshScreen():
+    self.screen.blit(background,(0,0))
+    pygame.display.flip()
+
+def setBackgroundImage(image):
+    self.background = pygame.image.load(image)
