4e319485b0b67c5a51199973ca6ce73bea93702e
Braedon Leonard <redirectmystuffplz@gmail.com>
2016-03-04 08:58:08 -0500

    Added player file

---
+++ Main.py
@@ -0,0 +1,22 @@
+#Main module to import when you start programming
+#Doesn't do anything yet
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+size = screenHeight, screenWidth = 1280, 960
+
+screen = pygame.display.set_mode(size)
+
+
+
+
+def mainMenu():
+
+    print ("This is where the main menu will run")
+
+def game():
+
+    print("This is where you'll run the main game")
