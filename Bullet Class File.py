810b04f66016b08d6d0729be8bc80e801adbce6a
SPuri98 <serena.t.puri@gmail.com>
2016-03-22 08:58:51 -0400

    Added files via upload

---
+++ Bullet Class File.py
@@ -0,0 +1,11 @@
+#Bullet Class File
+import pygame
+from pygame.locals import *
+from moving import moving
+pygame.init()
+
+
+class bullet(moving):
+
+    def __init__(self,x,y,image):    
+        moving.__init__(self,x,y,image)
